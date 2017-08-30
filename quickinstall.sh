4cfdb8772c1ab4048c9682b49b501928f94f33df
Ben Anhalt <anhalt@ku.edu>
2017-08-30 17:37:18 -0500

    fix echoing.

--- quickinstall.sh
+++ quickinstall.sh
@@ -4,47 +4,43 @@
 # complete license text
 # Source code at https://github.com/jgamblin/quickinstall
 
-echo -e "\nAdding emacs ppa...\n"
+echo  "\nAdding emacs ppa...\n"
 add-apt-repository -y ppa:ubuntu-elisp/ppa
 
 # Upgrade installed packages to latest
-echo -e "\nRunning a package upgrade...\n"
+echo  "\nRunning a package upgrade...\n"
 apt-get -qq update && apt-get -qq dist-upgrade
 
 
 # Install stuff I use all the time
-echo -e "\nInstalling default packages...\n"
+echo  "\nInstalling default packages...\n"
 apt-get -qq install fail2ban git unattended-upgrades ufw emacs-snapshot
 
 # Adjust unattended-upgrades settings
-echo -e "\nUnattended-Upgrade::Remove-Unused-Dependencies \"true\";\n" >> /etc/apt/apt.conf.d/50unattended-upgrades
-echo -e "\nUnattended-Upgrade::Automatic-Reboot \"true\";\n" >> /etc/apt/apt.conf.d/50unattended-upgrades
+echo  "\nUnattended-Upgrade::Remove-Unused-Dependencies \"true\";\n" >> /etc/apt/apt.conf.d/50unattended-upgrades
+echo  "\nUnattended-Upgrade::Automatic-Reboot \"true\";\n" >> /etc/apt/apt.conf.d/50unattended-upgrades
 
 
 #Install and configure firewall
-echo -e "\nConfiguring firewall...\n"
+echo  "\nConfiguring firewall...\n"
 ufw default deny incoming
 ufw default allow outgoing
 ufw allow ssh
-
-sed -i.bak 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
-chmod 0644 /etc/ufw/ufw.conf
-
-ufw enable
+ufw --force enable
 
 # set timezone to UTC
-echo -e "\nUpdating Timezone to UTC...\n"
+echo  "\nUpdating Timezone to UTC...\n"
 sudo timedatectl set-timezone UTC
 
 #Install Ruby
-#echo -e "\nInstalling Ruby...\n"
+#echo  "\nInstalling Ruby...\n"
 #curl -L https://get.rvm.io | bash -s stable --ruby
 
 #PCAP Everything
-#echo -e "\nRunning docker: pcap...\n"
+#echo  "\nRunning docker: pcap...\n"
 #docker run -v ~/pcap:/pcap --net=host -d jgamblin/tcpdump
 
-echo -e "\nAdding regular user...\n"
+echo  "\nAdding regular user...\n"
 adduser --disabled-password --gecos "" ben
 su -c 'mkdir -p -m700 /home/ben/.ssh && touch /home/ben/.ssh/authorized_keys' ben
 cat /root/.ssh/authorized_keys >> /home/ben/.ssh/authorized_keys
